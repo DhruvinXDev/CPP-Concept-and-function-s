4503a511d1d8434e639f502c4c4385333d6f02dc
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-03-23 22:18:07 +0530

    add file

--- Vector/fornt_back_at.cpp
+++ Vector/fornt_back_at.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main ( )
 {
     vector<int> vect ={1,2,3,4,5} ;
+    
     cout << vect.front( )<<endl; //to get first value
     cout << vect.back( )<<endl; // to get last value
     cout << vect.at(2)  << endl; //to get 2 index value
