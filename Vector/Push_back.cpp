ee5dc001af843fb4b11a54478832e1ffcb2813f6
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-03-23 22:16:58 +0530

    add file

--- Vector/Push_back.cpp
+++ Vector/Push_back.cpp
@@ -5,13 +5,10 @@ using namespace std;
 int main ( )
 {
     vector<int> vect;//size 0
-
     vect.push_back(10); // add 10 in the last {10}
     vect.push_back(20); //add 20 in the last {10 ,20}
     vect.push_back(30); // add 30 in the last {10,20,30}
-    cout <<"Size of vector is : "<<vect.size( )<<endl;
-
-    //vect.size() to get size of vector
+    cout <<"Size of vector is : "<<vect.size( )<<endl; //vect.size() to get size of vector
 
     //for each loop
     for( int i : vect ) // hear i is int because vect is int
