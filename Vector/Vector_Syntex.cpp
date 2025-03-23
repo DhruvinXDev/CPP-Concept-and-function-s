c8c50940b49bd47f1ee4acb953c0348c9a7b1d9a
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-03-23 22:15:25 +0530

    add file

--- Vector/Vector_Syntex.cpp
+++ Vector/Vector_Syntex.cpp
@@ -4,10 +4,8 @@ using namespace std;
 
 int main ( )
 {
-    vector<int> vec1 ;
-    
+    vector<int> vec1;    
     vector<int> vect2 = {1,2,3}; 
-
     vector<int> vect3(3,0); 
     //vect3(3,0) mean 3 is a size of vector and o is the initial value of the all index
 
