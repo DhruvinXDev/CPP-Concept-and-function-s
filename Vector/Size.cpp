6a3210b2bec5c4bb43c133b30acb642699ca5fb5
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-03-23 22:16:19 +0530

    Add file

--- Vector/Size.cpp
+++ Vector/Size.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int main ( )
 {
     vector<int> vect ={1,2,3,4,5} ;
-    cout <<"Size of vector is : "<<vect.size( )<<endl;
-
-    //vect.size() to get size of vector
+    cout <<"Size of vector is : "<<vect.size( )<<endl; //vect.size() to get size of vector
 
     //for each loop
     for( int i : vect ) // hear i is int because vect is int
