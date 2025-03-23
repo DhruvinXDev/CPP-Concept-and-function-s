0ec64180617a9468f8af626dc854e21d0d28f756
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-03-23 22:19:26 +0530

    add file

--- Vector/capacity.cpp
+++ Vector/capacity.cpp
@@ -6,7 +6,7 @@ int main ( )
 {
     vector<int> vect ={1,2,3,4,5} ;
     cout <<"Size of vector is : "<<vect.size( )<<endl;
-    cout <<"Capacity of vector is : "<< vect.capacity( ) <<endl; 
-    //capacity >= size 
+    cout <<"Capacity of vector is : "<< vect.capacity( )<<endl;
+    //capacity >= size
     return 0;
 }
